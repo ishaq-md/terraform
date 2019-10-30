7400c7a3d784f3ab775a52067427ac6936da09fd
unknown <ishaqmd@live.com>
2019-10-30 21:13:36 +0530

    eighth commit

--- 01-hello-world/main.tf
+++ 01-hello-world/main.tf
@@ -1,6 +1,5 @@
 # Configure the Google Cloud provider
 provider "google" {
-  credentials = file("cred.json")
   project = "silent-thunder-256105"
   region  = "us-east1-b"
 }
@@ -10,6 +9,7 @@ resource "google_compute_instance" "example" {
   name          = "example"
   machine_type  = "f1-micro"
   zone          = "us-east1-b"
+  tags = ["prod"]
   
   boot_disk {
     initialize_params {
